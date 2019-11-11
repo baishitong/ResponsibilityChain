6193b2f9505651f83eaed99237ec5f469d710acb
fbt <>
2019-11-11 18:28:37 +0800

    first commit

---
+++ ResponsibilityChain/Business/DefiningTermChain.m
@@ -0,0 +1,67 @@
+//
+//  DefiningTermChain.m
+//  NeoTV
+//
+//  Created by neotv on 2018/6/15.
+//  Copyright © 2018年 iOS. All rights reserved.
+//
+
+#import "DefiningTermChain.h"
+#import "DJQTextFiledTitleView.h"
+
+@interface DefiningTermChain ()
+
+@property (nonatomic, strong) NSString *errorMessage;
+
+@end
+
+@implementation DefiningTermChain
+
+- (ResponsibilityMessage *)canPassThrough {
+    
+    if ([self.object isKindOfClass:DJQTextFiledTitleView.class]){
+        DJQTextFiledTitleView *titleView  = self.object;
+        ResponsibilityMessage *message   = [ResponsibilityMessage new];
+        message.object                   = self.object;
+        message.checkSuccess             = YES;
+        
+        if ([titleView.modelDicKey isEqualToString:@"qq"]) {
+            if (titleView.field.text.length < 5) {
+                message.errorMessage = @"QQ号最低五位";
+                message.checkSuccess = NO;
+            }
+        }
+        
+        if (titleView.field.text.length <= 0) {
+            
+            message.errorMessage = [NSString stringWithFormat:@"%@输入不能为空", self.errorMessage];
+            message.checkSuccess = NO;
+            
+        }
+        
+        return message;
+    }else {
+        UITextField           *field   = self.object;
+        ResponsibilityMessage *message  = [ResponsibilityMessage new];
+        message.object                  = self.object;
+        message.checkSuccess           = YES;
+        
+        
+        if (field.text.length <= 0) {
+            
+            message.errorMessage = [NSString stringWithFormat:@"%@输入不能为空", self.errorMessage];
+            message.checkSuccess = NO;
+            
+        }
+        
+        return message;
+    }
+}
+
++ (instancetype)DefiningTermChainWithErrorMessage:(NSString *)string {
+    DefiningTermChain *chain = [DefiningTermChain new];
+    chain.errorMessage       = string;
+    
+    return chain;
+}
+@end
